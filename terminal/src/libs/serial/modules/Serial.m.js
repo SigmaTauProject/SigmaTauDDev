32ba31ee0fe3f856b47ccfa03048c3effd90ce4f
Jonathan <JonathanILevi@gmail.com>
2020-06-05 20:48:18 -0500

    JS Object/Struct serialization.
    Also:
    Refactored sizeof to sizeOf.

--- terminal/src/libs/serial/modules/Serial.m.js
+++ terminal/src/libs/serial/modules/Serial.m.js
@@ -13,10 +13,10 @@ const SerialType = {
 	float32	: "float32"	,
 	float64	: "float64"	,
 			
-	array	: (elementType, length=undefined)=>({type:"array", length:length, elementType})	,
-	staticArray	: (elementType, length)=>({type:"array", length:length, elementType})	,
-	struct	: Cls=>({type:"struct", Cls:cls})	,
-	object	: "object"	,
+	array	: (elementType, length=undefined)=>({type:"array", length, elementType})	,
+	staticArray	: (elementType, length)=>({type:"array", length, elementType})	,
+	struct	: Cls=>({type:"struct", Cls})	,
+	object	: Cls=>({type:"struct", Cls})	,
 };
 
 const basicTypes = [
@@ -32,15 +32,15 @@ const basicTypes = [
 	SerialType.float32	,
 	SerialType.float64	,
 ];
-const basicSizeof = {
+const basicSizeOf = {
 	uint8	: 1	,
 	int8	: 1	,
 	uint16	: 2	,
 	int16	: 2	,
 	uint32	: 4	,
 	int32	: 4	,
-	uint64	: 8	,
-	int64	: 8	,
+	bigUint64	: 8	,
+	bigInt64	: 8	,
 			
 	float32	: 4	,
 	float64	: 8	,
@@ -84,29 +84,39 @@ class Serializer {
 		return new Serializer(...this.attributes, ...moreAttributes);
 	}
 	
-	sizeof(type) {
+	sizeOf(type) {
 		if (type.constructor == Array)
-			return subserializer(...type.slice(1)).sizeof(type[0]);
+			return subserializer(...type.slice(1)).sizeOf(type[0]);
 		
-		if (basicSizeof[type] != undefined)
-			return basicSizeof[type];
+		if (basicSizeOf[type] != undefined)
+			return basicSizeOf[type];
 		if (typeof(type) == "object" && type.type == "array") {
 			if (type.length != undefined) {
-				let valueSize = this.sizeof(type.elementType);
+				let valueSize = this.sizeOf(type.elementType);
 				if (valueSize == undefined)
 					return undefined;
 				return valueSize*type.length;
 			}
 			return undefined;
 		}
-		console.assert(false, "Serialize Type not defined.");
+		if ((typeof(type) == "object" && type.type == "struct")) {
+			let valuesSize = 0;
+			for (let k of Object.keys(type.Cls).filter(k=>k.startsWith("serial_"))) {
+				let vs = this.sizeOf(type.Cls[k]);
+				if (vs == undefined)
+					return undefined;
+				valuesSize += vs;
+			}
+			return valuesSize;
+		}
+		return undefined;
 	}
-	byteSizeof(type, value) {
+	byteSizeOf(type, value) {
 		if (type.constructor == Array)
-			return subserializer(...type.slice(1)).byteSizeof(type[0]);
+			return subserializer(...type.slice(1)).byteSizeOf(type[0]);
 		
 		{
-			let size = this.sizeof(type);
+			let size = this.sizeOf(type);
 			if (size != undefined)
 				return  size;
 		}
@@ -120,30 +130,39 @@ class Serializer {
 			else {
 				if (this.getAttribute(NoLength))
 					return undefined;
-				lengthSize = this.byteSizeof(this.getAttribute(LengthType), value.length);
+				lengthSize = this.byteSizeOf(this.getAttribute(LengthType), value.length);
 				if (lengthSize == undefined)
 					return undefined;
 			}
 			
-			let valueSize = this.sizeof(type.elementType);
+			let valueSize = this.sizeOf(type.elementType);
 			if (valueSize != undefined)
 				return lengthSize + valueSize*value.length;
-			let valuesSize;
+			let valuesSize = 0;
 			for (let v of value) {
-				let vs = this.byteSizeof(type.elementType, v);
+				let vs = this.byteSizeOf(type.elementType, v);
 				if (vs == undefined)
 					return undefined;
 				valuesSize += vs;
 			};
 			return lengthSize + valuesSize;
 		}
+		if (typeof(type) == "object" && type.type == "struct") {
+			let valuesSize = 0;
+			for (let k of Object.keys(type.Cls).filter(k=>k.startsWith("serial_"))) {
+				let vs = this.byteSizeOf(type.Cls[k], value[k.slice("serial_".length)]);
+				if (vs == undefined)
+					return undefined;
+				valuesSize += vs;
+			}
+			return valuesSize;
+		}
+		console.assert(false, "Serialize Type not defined.");
 	}
 	
 	serialize(type, value, buffer=undefined) {
 		if (type.constructor == Array)
 			return subserializer(...type.slice(1)).serialize(type[0], value, buffer);
-		
-		if (buffer) console.assert(buffer.byteOffset + buffer.byteLength == buffer.buffer.byteLength, "`buffer` must be at the end of root `ArrayBuffer`.");
 		
 		function defineBuffer(size) {
 			buffer = buffer != undefined && buffer.byteLength >= size
@@ -152,7 +171,7 @@ class Serializer {
 		}
 		
 		if (basicTypes.includes(type)) {
-			defineBuffer(basicSizeof[type]);
+			defineBuffer(basicSizeOf[type]);
 			viewSet(dataView(buffer), type, value);
 			return buffer;
 		}
@@ -166,7 +185,7 @@ class Serializer {
 				noLength = this.getAttribute(NoLength);
 			}
 			
-			let byteLength = this.byteSizeof(type, value);
+			let byteLength = this.byteSizeOf(type, value);
 			if (byteLength) {
 				defineBuffer(byteLength);
 				let workingBuffer = buffer;
@@ -181,6 +200,29 @@ class Serializer {
 					builder.push(this.serialize(this.getAttribute(LengthType), value.length));
 				for (let v of value)
 					builder.push(this.serialize(type.elementType, v));
+				byteLength = builder.sum(b=>b.byteLength);
+				defineBuffer(byteLength);
+				let workingBuffer = buffer;
+				for (let b of builder) {
+					this._copyTo(b, workingBuffer);
+				}
+			}
+			return buffer;
+		}
+		if (typeof(type) == "object" && type.type == "struct") {
+			let keys = Object.keys(type.Cls).filter(k=>k.startsWith("serial_")).map(k=>k.slice("serial_".length));
+			
+			let byteLength = this.byteSizeOf(type, value);
+			if (byteLength) {
+				defineBuffer(byteLength);
+				let workingBuffer = buffer;
+				for (let k of keys)
+					workingBuffer = this._serializeTo(type.Cls["serial_"+k], value[k], workingBuffer);
+			}
+			else {
+				let builder = [];
+				for (let v of value)
+					builder.push(this.serialize(type.Cls["serial_"+k], value[k]));
 				byteLength = builder.sum(b=>b.byteLength);
 				defineBuffer(byteLength);
 				let workingBuffer = buffer;
@@ -206,7 +248,7 @@ class Serializer {
 			return subserializer(...type.slice(1)).deserialize(type[0], buffer, workingBuffer_callback);
 		
 		if (basicTypes.includes(type)) {
-			workingBuffer_callback(chopBuffer(buffer, basicSizeof[type]));
+			workingBuffer_callback(chopBuffer(buffer, basicSizeOf[type]));
 			return viewGet(dataView(buffer), type);
 		}
 		if (typeof(type) == "object" && type.type == "array") {
@@ -230,6 +272,13 @@ class Serializer {
 				workingBuffer_callback(buffer);
 				return value;
 			}
+		}
+		if (typeof(type) == "object" && type.type == "struct") {
+			let value = new type.Cls;
+			let keys = Object.keys(type.Cls).filter(k=>k.startsWith("serial_")).map(k=>k.slice("serial_".length));
+			for (let k of keys)
+				value[k] = this.deserialize(type.Cls["serial_"+k], buffer, wb=>buffer=wb)
+			return value;
 		}
 		console.assert(false, "Serialize Type not defined.");
 	}
