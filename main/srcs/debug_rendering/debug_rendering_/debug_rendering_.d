71b82f3d008b509405f725bfd509f0e71b4e0ab1
Jonathan <JonathanILevi@gmail.com>
2022-03-02 21:48:08 -0600

    Debug Rendering: Selecting (including code improvements)

--- main/srcs/debug_rendering/debug_rendering_/debug_rendering_.d
+++ main/srcs/debug_rendering/debug_rendering_/debug_rendering_.d
@@ -1,5 +1,7 @@
 module debug_rendering_.debug_rendering_; 
 
+import std.stdio;
+
 import std.algorithm;
 
 import bindbc.sdl;
@@ -11,15 +13,40 @@ import math.linear.point;
 
 import updaterate_;
 
+alias ScreenPos = PVec2!ScreenPosT;	alias ScreenPosT = int;
+
+struct RRect {
+	union {
+		struct {
+			int x;
+			int y;
+			int w;
+			int h;
+		}
+		struct {
+			ScreenPos pos;
+			int[2] size;
+		}
+		Rect rect;
+	}
+}
+
 class DebugRendering {
 	Window* window;
 	Renderer* renderer;
 	
-	float zoom;
-	RelPos pos;
-	Viewer viewer;
+	View view;
+	Selector selector;
 	
-	class Viewer {
+	class View {
+		float zoom;
+		RelPos pos;
+		
+		this() {
+			pos = pvec(0f,0);
+			zoom = 0.0001;
+		}
+		
 		void event(Event event) {
 			if (event.type == mousewheel) {
 				if (event.wheel.y > 0) {
@@ -31,14 +58,37 @@ class DebugRendering {
 			}
 			else if (event.type == mousemotion) {
 				if (event.motion.state & buttonLmask) {
-					pos.x -= event.motion.xrel/zoom*2;
-					pos.y -= event.motion.yrel/zoom*2;
+					view.pos.x -= event.motion.xrel/zoom*2;
+					view.pos.y -= event.motion.yrel/zoom*2;
+				}
+			}
+		}
+		
+		ScreenPos toScreenPos(WorldPos pos) {
+			return	point(((pos-view.pos.vector).vector * view.zoom).castType!int+renderSize/2);
+		}
+		WorldPos fromScreenPos(ScreenPos pos) {
+			return point(((pos-renderSize/2).vector / view.zoom + view.pos.vector).castType!WorldPosT);
+		}
+	}
+	class Selector {
+		Entity[] selected;
+		void event(Event event) {
+			if (event.type == mousebuttondown) {
+				selected.length = 0;  selected.assumeSafeAppend;
+				auto pos = view.fromScreenPos(pvec(event.button.x, event.button.y));
+				foreach (entity; world.physicsWorld.entities) {
+					auto r = max(2, cast(int)(entity.object.broadRadius*0.7));
+					if (pos.x >= entity.pos.x-r && pos.x <= entity.pos.x+r && pos.y >= entity.pos.y-r && pos.y <= entity.pos.y+r) {
+						writeln(entity);
+						selected ~= entity;
+					}
 				}
 			}
 		}
 	}
 	
-	int[2] renderSize;
+	Vec2!int renderSize;
 	
 	World world;
 	
@@ -46,9 +96,8 @@ class DebugRendering {
 	
 	this(World world) {
 		this.world = world;
-		pos = pvec(0f,0);
-		zoom = 0.0001;
-		viewer = new Viewer;
+		view = new View;
+		selector = new Selector;
 		
 		window = createWindow("Stigma", 
 			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -61,7 +110,7 @@ class DebugRendering {
 			RendererFlags.accelerated,
 		);
 		
-		renderer.getRendererOutputSize(&renderSize[0],&renderSize[1]);
+		renderer.getRendererOutputSize(&renderSize.x, &renderSize.y);
 	}
 	~this() {
 		window.destroyWindow;
@@ -70,12 +119,12 @@ class DebugRendering {
 	
 	void update() {
 		for (Event event; pollEvent(&event);) {
-			if (event.type == cast(SDL_EventType)EventType.quit) {
+			if (event.type == cast(SDL_EventType) EventType.quit) {
 			}
 			else if (event.type == windowevent) {
 				if (event.window.event == windoweventResized) {
 					window.setWindowSize(event.window.data1, event.window.data2);
-					renderer.getRendererOutputSize(&renderSize[0],&renderSize[1]);
+					renderer.getRendererOutputSize(&renderSize.x,&renderSize.y);
 				}
 			}
 			else {
@@ -104,7 +153,8 @@ class DebugRendering {
 						writeln(updaterate);
 					}
 				}
-				viewer.event(event);
+				view.event(event);
+				selector.event(event);
 			}
 		}
 		
@@ -113,9 +163,9 @@ class DebugRendering {
 		
 		foreach (entity; world.physicsWorld.entities) {
 			renderer.setRenderDrawColor(0,255,255, 255);
-			drawSpot(entity.pos, max(2, cast(int)(entity.object.broadRadius*zoom*1.4)));
+			drawSpot(entity.pos, max(2, cast(int)(entity.object.broadRadius*view.zoom*1.4)), selector.selected.canFind(entity));
 			if (entity.object == fineShipObject) {
-				import std.stdio; if (entity.trajectory.length == 0) writeln("Fully recreating trajectory!");
+				////import std.stdio; if (entity.trajectory.length == 0) writeln("Fully recreating trajectory!");
 				foreach_reverse (i,traj; entity.traject(world.physicsWorld.gravityWells, 16*16*16)) {
 					renderer.setRenderDrawColor(cast(ubyte)max(16,min(255*2 - cast(long)i/2, 255)),cast(ubyte)max(0,255 - cast(long)i/2),0, 255);
 					drawSpot(traj, 2);
@@ -129,8 +179,14 @@ class DebugRendering {
 			update;
 	}
 	
-	void drawSpot(WorldPos p, int size) {
-		Rect rect = {cast(int)((p.x-pos.x)*zoom-size/2)+renderSize[0]/2, cast(int)((p.y-pos.y)*zoom-size/2)+renderSize[1]/2, size, size};
-		renderer.renderDrawRect(&rect);
+	void drawSpot(WorldPos pos, int size, bool fill=false) {
+		RRect r;
+		r.pos = view.toScreenPos(pos) - size/2;
+		r.w = size; r.h = size;
+		
+		if (fill)
+			renderer.renderFillRect(&r.rect);
+		else
+			renderer.renderDrawRect(&r.rect);
 	}
 }
